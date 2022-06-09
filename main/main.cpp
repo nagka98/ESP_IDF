afe3c3c34f3f9945c7bfeab2da676a93decffd51
larax476 <larax476@users.noreply.github.com>
2022-06-09 20:46:07 +0200

    added kalman filter to initial ESP code

--- main/main.cpp
+++ main/main.cpp
@@ -441,15 +441,10 @@ void create_result_string(int64_t timestamp) {
         for(i = 0; i < NUM_CHANNELS; i++) {
             int R = R_values[i];
             if(R_values[i] <= 0 || R_values[i] > 1000000) {
-                ptr += sprintf(ptr, "0,");//remove comments 
+                ptr += sprintf(ptr, "0,");
             } else {
-            //      //ptr += sprintf(ptr, "%d,", R);//remove comments
-                  ptr += sprintf(ptr, "%d,",(int)round(kalman_predict(m_kf_r[i],(float)(R))));//remove comments         
+                  ptr += sprintf(ptr, "%d,",(int)round(kalman_predict(m_kf_r[i],(float)(R))));       
             }
-            // if(i != 9)
-            // {
-            //      ptr += sprintf(ptr, ",");
-            // }  
         }
     #endif
     #ifdef AD5933_ADDR
@@ -457,29 +452,19 @@ void create_result_string(int64_t timestamp) {
         //ptr += sprintf(ptr, "\n");
         for(i = 0; i < NUM_CHANNELS; i++) {
             int Z = Z_values[i];
-            if(Z <= 0 || Z > 1000000) {// set this to 500000
-                 //ptr += sprintf(ptr, "0,");//remove comments
+            if(Z <= 0 || Z > 500000) {
+                 ptr += sprintf(ptr, "0,");
             } else {
-                 //ptr += sprintf(ptr, "%d,", Z);//remove comments
-                 //ptr += sprintf(ptr, "%d,",(int)round(kalman_predict(m_kf_r[i],(float)(Z))));//remove comments
+                 ptr += sprintf(ptr, "%d,",(int)round(kalman_predict(m_kf_r[i],(float)(Z))));
             }
         }
     #endif
     #ifdef LSM6_ADDR
-       //ptr += sprintf(ptr, "%f,%f,%f,", xl.x*(convert_g_to_ms2), xl.y*(convert_g_to_ms2), xl.z*(convert_g_to_ms2));
-       //ptr += sprintf(ptr, "%f,%f,%f,", gy.x*(convert_g_to_ms2), gy.y*(convert_g_to_ms2), gy.z*(convert_g_to_ms2));
-       ptr += sprintf(ptr, "%d,%d,%d", (int)kalman_predict(m_kf_acc[0],xl.x*(convert_g_to_ms2)), (int)kalman_predict(m_kf_acc[1],xl.y*(convert_g_to_ms2)), (int)kalman_predict(m_kf_acc[2],xl.z*(convert_g_to_ms2)));
-       //ptr += sprintf(ptr, "%d,%d,%d,", (int)kalman_predict(m_kf_gyr[0],gy.x*(convert_g_to_ms2)), (int)kalman_predict(m_kf_gyr[1],gy.y*(convert_g_to_ms2)), (int)kalman_predict(m_kf_gyr[2],gy.z*(convert_g_to_ms2)));
-        const FusionVector gyroscope = {(float)gy.x,(float)gy.y,(float)gy.z};
-        const FusionVector accelerometer = {(float)xl.x,(float)xl.y,(float)xl.z};
-        FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, SAMPLE_PERIOD);
-        const FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
-        //ptr += sprintf(ptr, "%f,%f,%f", euler.angle.roll, euler.angle.pitch, euler.angle.yaw);
-
+       ptr += sprintf(ptr, "%f,%f,%f,", xl.x, xl.y, xl.z);
+       ptr += sprintf(ptr, "%f,%f,%f,", gy.x, gy.y, gy.z);
     #endif
     #ifdef LIS2_ADDR
-       //ptr += sprintf(ptr, "%f,%f,%f", mag.x*(convert_g_to_ms2), mag.y*(convert_g_to_ms2), mag.z*(convert_g_to_ms2));
-       //ptr += sprintf(ptr, "%d,%d,%d", (int)kalman_predict(m_kf_mag[0],mag.x*(convert_g_to_ms2)), (int)kalman_predict(m_kf_mag[1],mag.y*(convert_g_to_ms2)), (int)kalman_predict(m_kf_mag[2],mag.z*(convert_g_to_ms2)));
+       ptr += sprintf(ptr, "%f,%f,%f", mag.x, mag.y, mag.z);
     #endif
 }
 
